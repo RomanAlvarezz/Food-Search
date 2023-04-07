496f72b35824d18b642db4cf965e626c201bb608
Roman <alvarezroman150@gmail.com>
2023-04-07 16:46:15 -0300

    slider component styles

--- app/index.js
+++ app/index.js
@@ -15,3 +15,23 @@ document.addEventListener('click', e => {
     }
     console.log(e.target)
 })
+
+const swiper = new Swiper('.swiper', {
+    speed: 600,
+    spaceBetween: 10,
+    slidesPerView: 1,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        450: {
+            slidesPerView: 2,
+            spaceBetween: 10
+        },
+        750: {
+            slidesPerView: 3,
+            spaceBetween: 15
+        }
+    }
+  });
