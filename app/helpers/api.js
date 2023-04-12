663a2026915cb7cde874f38a093d02066fb08e8c
Roman <alvarezroman150@gmail.com>
2023-04-11 23:21:13 -0300

    cambie el justify content de la caja de opciones del filtro

---
+++ app/helpers/api.js
@@ -0,0 +1,13 @@
+const CUISINE = ['african', 'american', 'british', 'cajun', 'caribbean', 'chinese', 'eastern european', 'european',
+'french', 'german', 'greek', 'indian', 'irish', 'italian', 'japanese', 'jeweish', 'korean', 'latin american', 'mediterranean', 'mexican', 'meddle eastern', 'nordic', 'southern', 'spanish', 'thai', 'vietnamese'],
+ TYPE = ['main course', 'side dish', 'dessert', 'appetizer', 'salad', 'bread', 'breakfast', 'soup', 'beverage', 'sauce', 'marinade', 'fingerfood', 'snack', 'drink'],
+ DIET = ['gluten free', 'ketogenic', 'vegetarian', 'Lacto-Vegetarian', 'Ovo-Vegetarian', 'Vegan', 'Pescetarian', 'Paleo', 'Primal', 'Low FODMAP','Whole30'
+],
+ INTOLERANCES = ['dairy', 'egg', 'gluten', 'grain', 'peanut', 'seafood', 'sesame', 'shellfish', 'soy', 'sulfite', 'tree Nut', 'wheat'];
+
+ export default {
+    CUISINE,
+    TYPE,
+    DIET,
+    INTOLERANCES
+ }
