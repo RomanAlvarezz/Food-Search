a88e2f4d922a5c2e2135b3166fa67c13f115cd76
Roman <alvarezroman150@gmail.com>
2023-04-11 23:18:18 -0300

    funcionamiento basico del filtro

--- app/components/Filter.js
+++ app/components/Filter.js
@@ -1,3 +1,5 @@
+import api from '../helpers/api.js';
+
 const d = document;
 
 export function Filter() {
@@ -13,62 +15,88 @@ export function Filter() {
     </div> 
     <div class="filter__content">
         <button class="filter__btn-info"><p>diet</p><p>info</p></button>
-        <span class="filter__option filter__option-active">african</span>
-        <span class="filter__option">american</span>
-        <span class="filter__option">british</span>
-        <span class="filter__option">cajun</span>
-        <span class="filter__option">caribbean</span>
-        <span class="filter__option">chinese</span>
-        <span class="filter__option">eastern</span>
-        <span class="filter__option">european</span>
-        <span class="filter__option">french</span>
-        <span class="filter__option">greek</span>
-        <span class="filter__option">indian</span>
-        <span class="filter__option">irish</span>
-        <span class="filter__option">italian</span>
-        <span class="filter__option">japanese</span>
-        <span class="filter__option">jewish</span>
-        <span class="filter__option">korean</span>
-        <span class="filter__option">latin american</span>
-        <span class="filter__option">mediterranean</span>
-        <span class="filter__option">mexican</span>
-        <span class="filter__option">middle eastern</span>
-        <span class="filter__option">nordic</span>
-        <span class="filter__option">southern</span>
-        <span class="filter__option">spanish</span>
-        <span class="filter__option">thai</span>
-        <span class="filter__option">vietnamese</span>
+        <span class="filter__option filter__option-active" data-state="active">african</span>
+        <span class="filter__option" data-state="inactive">american</span>
+        <span class="filter__option" data-state="inactive">british</span>
+        <span class="filter__option" data-state="inactive">cajun</span>
+        <span class="filter__option" data-state="inactive">caribbean</span>
+        <span class="filter__option" data-state="inactive">chinese</span>
+        <span class="filter__option" data-state="inactive">eastern</span>
+        <span class="filter__option" data-state="inactive">european</span>
+        <span class="filter__option" data-state="inactive">french</span>
+        <span class="filter__option" data-state="inactive">greek</span>
+        <span class="filter__option" data-state="inactive">indian</span>
+        <span class="filter__option" data-state="inactive">irish</span>
+        <span class="filter__option" data-state="inactive">italian</span>
+        <span class="filter__option" data-state="inactive">japanese</span>
+        <span class="filter__option" data-state="inactive">jewish</span>
+        <span class="filter__option" data-state="inactive">korean</span>
+        <span class="filter__option" data-state="inactive">latin american</span>
+        <span class="filter__option" data-state="inactive">mediterranean</span>
+        <span class="filter__option" data-state="inactive">mexican</span>
+        <span class="filter__option" data-state="inactive">middle eastern</span>
+        <span class="filter__option" data-state="inactive">nordic</span>
+        <span class="filter__option" data-state="inactive">southern</span>
+        <span class="filter__option" data-state="inactive">spanish</span>
+        <span class="filter__option" data-state="inactive">thai</span>
+        <span class="filter__option" data-state="inactive">vietnamese</span>
     </div>    
     `;
 
-    d.addEventListener('click', e => {
-        let $element = e.target;
-
-        if ($element.matches('.filter__btn-info') || $element.matches('.filter__btn-info *')){
-            d.querySelector('.diets__modal').style.transform = 'translateX(0)';
-            return;
-        }
-
-        if ($element.classList.contains('filter__category') &&
-        !($element.classList.contains('filter__category-active'))) {
-            d.querySelector('.filter__category-active').classList.remove('filter__category-active');
-            $element.classList.add('filter__category-active');
-            return;
-        }
-
-        if ($element.classList.contains('filter__option') &&
-        $element.classList.contains('filter__option-active')) {
-            $element.classList.remove('filter__option-active');
-            console.log('Desactivamos');
-            return;
-        }
-
-        if ($element.classList.contains('filter__option') &&
-        !($element.classList.contains('filter__option-active'))){
-            $element.classList.add('filter__option-active');
-            console.log('Activamos');
-        }
-    })
-
     return $filter;
 }
+
+
+
+function categorySelected(category) {
+    let html = '';
+    const $filterContent = d.querySelector('.filter__content');
+
+    switch (category) {
+        case 'CUISINE':
+            api[category].forEach(textItem => html += `<span class="filter__option" data-state="inactive">${textItem}</span>`);
+            break;
+        case 'DIET':
+            api[category].forEach(textItem => html += `<span class="filter__option" data-state="inactive">${textItem}</span>`);
+            break;
+        case 'TYPE':
+            api[category].forEach(textItem => html += `<span class="filter__option" data-state="inactive">${textItem}</span>`);
+            break;
+        case 'INTOLERANCES':
+            api[category].forEach(textItem => html += `<span class="filter__option" data-state="inactive">${textItem}</span>`);
+            break;
+    }
+
+    $filterContent.innerHTML = html;
+}
+
+function optionSelected($op) {
+    if ($op.matches('.filter__option-active')) {
+        $op.classList.remove('filter__option-active');
+    } else {
+        $op.classList.add('filter__option-active');
+    }
+}
+
+d.addEventListener('click', e => {
+    let $element = e.target;
+
+    if ($element.matches('.filter__btn-info') || $element.matches('.filter__btn-info *')){
+        d.querySelector('.diets__modal').style.transform = 'translateX(0)';
+        return;
+    }
+
+    if ($element.classList.contains('filter__category') &&
+    !($element.classList.contains('filter__category-active'))) {
+        d.querySelector('.filter__category-active').classList.remove('filter__category-active');
+        $element.classList.add('filter__category-active');
+
+        categorySelected($element.textContent.toUpperCase());
+
+        return;
+    }
+
+    if ($element.matches('.filter__option')){
+        optionSelected($element);
+    }
+})
