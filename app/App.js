c0bc8331a72e6c668192f0b5f400ca0fd925d9f1
Roman <alvarezroman150@gmail.com>
2023-04-17 18:53:19 -0300

    arreglo de cargas

--- app/App.js
+++ app/App.js
@@ -4,6 +4,7 @@ import { Main } from './components/Main.js';
 import { Saved } from './components/Saved.js';
 import { Diets } from './components/Diets.js';
 import { Footer } from './components/Footer.js';
+import { Router } from './components/Router.js';
 
 export function App() {
     const $root = document.getElementById('root');
@@ -12,7 +13,9 @@ export function App() {
     $root.appendChild(Header());
     $root.appendChild(Panel());
     $root.appendChild(Main());
-    $root.insertAdjacentElement('afterend', Saved());
-    $root.insertAdjacentElement('afterend', Diets());
-    $root.insertAdjacentElement('afterend', Footer());
+    $root.appendChild(Saved());
+    $root.appendChild(Diets());
+    $root.appendChild(Footer());
+
+    Router();
 }
