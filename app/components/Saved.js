5f6f40882f394ffd1051e49360250f121bcecbe7
Roman <alvarezroman150@gmail.com>
2023-04-20 15:09:18 -0300

    estilos del icono para eliminar elementos de la seccion saved

--- app/components/Saved.js
+++ app/components/Saved.js
@@ -8,30 +8,36 @@ export function Saved() {
     <aside class="saved">
         <img class="saved__btn-close" src="./app/assets/icon-close.svg" alt="close icon">
         <h2 class="saved__title">saved recipes</h2>
-        <article class="saved__card">
+        <a class="saved__card" href="#/recipe?id=789456">
             <img class="saved__card__image" src="https://fastly.picsum.photos/id/212/200/200.jpg?hmac=U4JUx4PJyTuKdZEPAk2Cw01YZM8rOypF8fTTO39POko" alt="card image">
             <h3 class="saved__card__title">Pasta with Garlic, Scallions, Cauliflower</h3>
-        </article>
-        <article class="saved__card">
+            <img class="saved__card__delete" data-id="789456" src="./app/assets/icon-trash.svg">
+        </a>
+        <a class="saved__card" href="#/recipe?id=789456">
             <img class="saved__card__image" src="https://fastly.picsum.photos/id/212/200/200.jpg?hmac=U4JUx4PJyTuKdZEPAk2Cw01YZM8rOypF8fTTO39POko" alt="card image">
             <h3 class="saved__card__title">Pasta with Garlic, Scallions, Cauliflower</h3>
-        </article>
-        <article class="saved__card">
+            <img class="saved__card__delete" data-id="789456" src="./app/assets/icon-trash.svg">
+        </a>
+        <a class="saved__card" href="#/recipe?id=789456">
             <img class="saved__card__image" src="https://fastly.picsum.photos/id/212/200/200.jpg?hmac=U4JUx4PJyTuKdZEPAk2Cw01YZM8rOypF8fTTO39POko" alt="card image">
             <h3 class="saved__card__title">Pasta with Garlic, Scallions, Cauliflower</h3>
-        </article>
-        <article class="saved__card">
+            <img class="saved__card__delete" data-id="789456" src="./app/assets/icon-trash.svg">
+            </a>
+        <a class="saved__card" href="#/recipe?id=789456">
             <img class="saved__card__image" src="https://fastly.picsum.photos/id/212/200/200.jpg?hmac=U4JUx4PJyTuKdZEPAk2Cw01YZM8rOypF8fTTO39POko" alt="card image">
             <h3 class="saved__card__title">Pasta with Garlic, Scallions, Cauliflower</h3>
-        </article>
-        <article class="saved__card">
+            <img class="saved__card__delete" data-id="789456" src="./app/assets/icon-trash.svg">
+        </a>
+        <a class="saved__card" href="#/recipe?id=789456">
             <img class="saved__card__image" src="https://fastly.picsum.photos/id/212/200/200.jpg?hmac=U4JUx4PJyTuKdZEPAk2Cw01YZM8rOypF8fTTO39POko" alt="card image">
             <h3 class="saved__card__title">Pasta with Garlic, Scallions, Cauliflower</h3>
-        </article>
-        <article class="saved__card">
+            <img class="saved__card__delete" data-id="789456" src="./app/assets/icon-trash.svg">
+        </a>
+        <a class="saved__card" href="#/recipe?id=789456">
             <img class="saved__card__image" src="https://fastly.picsum.photos/id/212/200/200.jpg?hmac=U4JUx4PJyTuKdZEPAk2Cw01YZM8rOypF8fTTO39POko" alt="card image">
             <h3 class="saved__card__title">Pasta with Garlic, Scallions, Cauliflower</h3>
-        </article>
+            <img class="saved__card__delete" data-id="789456" src="./app/assets/icon-trash.svg">
+        </a>
     </aside>
     `;
 
