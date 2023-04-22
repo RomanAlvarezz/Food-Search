5771f948a8810c84bb693dee4b841eca5903a55c
Roman <alvarezroman150@gmail.com>
2023-04-21 21:41:17 -0300

    persistencia de datos en Fridge

--- app/components/Fridge.js
+++ app/components/Fridge.js
@@ -19,27 +19,56 @@ export function Fridge() {
     <div class="fridge__ingredients__container">
         <h2 class="fridge__ingredients__title">your pantry ingredients</h2>
         <div class="fridge__ingredients">
-            <span class="fridge__ingredient">Tomato<i class="fa-solid fa-xmark fridge__ingredient-xmark"></i></span>
-            <span class="fridge__ingredient">Pepper<i class="fa-solid fa-xmark fridge__ingredient-xmark"></i></span>
-            <span class="fridge__ingredient">Meat<i class="fa-solid fa-xmark fridge__ingredient-xmark"></i></span>
+            
         </div>
     </div>
     `;
 
+
+    setTimeout(renderIngredients, 10);
     return $fridge;
+}
+
+
+function renderIngredients() {
+    let html = '';
+    const $ingredientsContainer = d.querySelector('.fridge__ingredients');
+    console.log($ingredientsContainer);
+    $ingredientsContainer.innerHTML = '';
+    const fridgeArr = JSON.parse(localStorage.getItem('fridge')) || [];
+
+    if (!(fridgeArr.length === 0)) {
+        fridgeArr.forEach(ingredient => html += `
+        <span class="fridge__ingredient">
+            ${ingredient}
+            <i data-ingredient="${ingredient}" class="fa-solid fa-xmark fridge__ingredient-xmark"></i>
+        </span>`);
+    }
+
+    $ingredientsContainer.innerHTML = html;
 }
 
 d.addEventListener('click', e => {
     let $element = e.target;
 
     if($element.matches('.fridge__ingredient-xmark')) {
+        const fridgeArr = JSON.parse(localStorage.getItem('fridge')) || [];
+        const index = fridgeArr.indexOf($element.dataset.ingredient);
+        fridgeArr.splice(index,1);
+        localStorage.setItem('fridge', JSON.stringify(fridgeArr));
+        //renderIngredients();
         $element.parentElement.remove();
     }
 
     if ($element.matches('.fridge__input-btn')) {
         const $input = d.querySelector('.fridge__input')
         let ingredient = $input.value.trim();
+
         if (ingredient) {
+            const fridgeArr = JSON.parse(localStorage.getItem('fridge')) || [];
+            fridgeArr.push(ingredient);
+            localStorage.setItem('fridge', JSON.stringify(fridgeArr));
+            //renderIngredients();
             const $ingredients = d.querySelector('.fridge__ingredients');
             $ingredients.insertAdjacentHTML('afterbegin', `<span class="fridge__ingredient">${ingredient}<i class="fa-solid fa-xmark fridge__ingredient-xmark"></i></span>`);
         }
