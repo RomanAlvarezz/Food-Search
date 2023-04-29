e94f0731817fe6c40487e29f746fffacbfc58608
Roman <alvarezroman150@gmail.com>
2023-04-29 16:05:14 -0300

    cambio en la descripcion de fridge

--- app/components/Fridge.js
+++ app/components/Fridge.js
@@ -7,7 +7,7 @@ export function Fridge() {
     $fridge.innerHTML = `
     <div class="fridge__text">
         <h1 class="fridge__title">Cook with what you have!</h1>
-        <p class="fridge__info">Don't want to go to the store? No problem! Enter the ingredients you have on hand, and we'll show you recipes you could make. For best results, enter 10 or more ingredients below, including staples like salt, pepper, and olive oil. The ingredients you add here will sync with your Virtual Pantry on Android. Any changes in one will update the other</p>
+        <p class="fridge__info">Don't want to go to the store? No problem! Enter the ingredients you have on hand, and we'll show you recipes you could make. For best results, enter 5 or more ingredients below, including staples like salt, pepper, and olive oil.</p>
     </div>
     <div class="fridge__mid">
         <div class="fridge__input-container">
@@ -17,7 +17,7 @@ export function Fridge() {
         <button class="fridge__btn">get recipe</button>
     </div>
     <div class="fridge__ingredients__container">
-        <h2 class="fridge__ingredients__title">your pantry ingredients</h2>
+        <h2 class="fridge__ingredients__title">your ingredients</h2>
         <div class="fridge__ingredients">
             
         </div>
